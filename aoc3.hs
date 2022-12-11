0294cbf8aac339d264218ace239af89b1617b8c3
harm <el.harmo@gmail.com>
2022-12-11 14:24:27 +0100

    day 11 part 2

--- aoc3.hs
+++ aoc3.hs
@@ -5,14 +5,13 @@ day11 = do
     let monkeys =  parse11 bundled []
     let (x:y:_) =  take 2 $ reverse $ sort $ map monkeyToScore $ last $ take 21 $ iterate (round' (`div` 3) ) monkeys
     print (x*y)
-    -- print $ map monkeyToScore $ last $ take 1001 $ iterate (round' id ) monkeys
     let (x':y':_) =  take 2 $ reverse $ sort $ map monkeyToScore $ last $ take 10001 $ iterate (round' id ) monkeys
     print (x'*y')
     return ()
 
 type Item = Integer
 type Operation = (Integer-> Integer)
-type Test = (Item -> Bool)
+type Test = Integer
 type MonkeyIndex = Int
 type ItemsInspected = Int
 data Monkey = M [Item] Operation Test MonkeyIndex MonkeyIndex ItemsInspected
@@ -21,7 +20,7 @@ instance Show Monkey where
     show (M items _ _ to to' _) = "Monkey " ++ show items ++ " true to: " ++ show to ++ " false to: " ++ show to'
 
 defaultMonkey :: Monkey
-defaultMonkey= M [] (const 0) (const False) 0 0 0
+defaultMonkey= M [] (const 0) 0 0 0 0
 
 parse11 :: [String] -> [Monkey] -> [Monkey]
 parse11 [] acc = reverse acc
@@ -50,26 +49,29 @@ opParse (_:_:_:_:"*":m:_) = (* read m)
 opParse _ = error "11.1"
 
 testParse :: [String] -> Test
-testParse words n = n `mod` read (last words) == 0
+testParse words = read (last words)
 
 indexParse :: [String] -> Int
 indexParse words = read (last words)
 
 
 round' :: (Integer->Integer) ->  [Monkey] -> [Monkey]
-round' f = roundKeepAway f 0
-roundKeepAway :: (Integer->Integer) ->  Int -> [Monkey] -> [Monkey]
-roundKeepAway f index monkeys  | index == length  monkeys = monkeys
-                    | otherwise                           = roundKeepAway f (index + 1) (handleMonkey f (monkeys !! index) index monkeys) 
+round' f monkeys = roundKeepAway f 0 (monkeySafe monkeys) monkeys
+roundKeepAway :: (Integer->Integer) ->  Int -> Test -> [Monkey] -> [Monkey]
+roundKeepAway f index safe monkeys  | index == length  monkeys = monkeys
+                    | otherwise                           = roundKeepAway f (index + 1) safe (handleMonkey f safe (monkeys !! index) index monkeys) 
 
-handleMonkey :: (Integer->Integer) -> Monkey -> Int -> [Monkey] -> [Monkey]
-handleMonkey _ (M [] _ _ _ _ _ ) _ acc = acc
-handleMonkey f (M (i:is) op t ti fi ii) ownIndex acc = handleMonkey f  monkey ownIndex updated
+handleMonkey :: (Integer->Integer) -> Test -> Monkey -> Int -> [Monkey] -> [Monkey]
+handleMonkey _ _ (M [] _ _ _ _ _ ) _ acc = acc
+handleMonkey f safe (M (i:is) op t ti fi ii) ownIndex acc = handleMonkey f safe  monkey ownIndex updated
     where monkey = M is op t ti fi (ii+1)
-          newWorryLevel = f $ op i
-          updateIndex = if t newWorryLevel then ti else fi
+          newWorryLevel = let x = f $ op i in  x `rem` safe
+          updateIndex = if newWorryLevel `mod` t == 0 then ti else fi
           updatedMonkey = let (M i o t mit mif ii) = acc !! updateIndex in M (i++[newWorryLevel]) o t mit mif ii
           updated = [if i == ownIndex then monkey else if i == updateIndex then updatedMonkey else m | (m,i) <- zip acc [0..] ]
 
 monkeyToScore :: Monkey -> Int
 monkeyToScore (M _ _ _ _ _ ii) = ii
+
+monkeySafe :: [Monkey] -> Test
+monkeySafe ms = product $ map (\(M _ _ t _ _ _) -> t) ms
