78303fe968b8be42dda1468b8c4256998ebdb7c5
harm <el.harmo@gmail.com>
2022-12-12 15:30:31 +0100

    12

--- aoc4.hs
+++ aoc4.hs
@@ -9,22 +9,16 @@ main = day12
 day12 = do
     content <- readFile "input12.txt"
     let coords = parse12' content
-    -- print coords
     print $ length coords
     let mapped = toMap coords
-    -- print "-----------------------------------"
-    -- print mapped
     let start = getCoord $ head $ filter (\n -> getType n == Start) coords
     let end = getCoord $ head $ filter (\n -> getType n == End) coords
-    -- print start
-    -- print end
-    -- print $ map getCoord coords
     let withOutgoingEdges = addEdges mapped
-    -- print "-----------------------------------"
-    -- print  withOutgoingEdges
-    -- print $ withOutgoingEdges ! (44,28)
     print "-----------------------------------"
     print $ dfsish' start withOutgoingEdges end
+    print "-----------------------------------"
+    let starts = map getCoord $ filter (\n -> getHeight n == 'a') coords
+    print $ minimum $ map (\s -> dfsish' s withOutgoingEdges end) starts
     return ()
 
 
@@ -55,7 +49,6 @@ mapFn :: Map Coordinate Node -> ((Int, Int), Node) -> ((Int, Int), Node)
 mapFn mapped ((x,y),N c i t ns) = ((x,y),N c i t edges)
     where adj = filter (`member` mapped) [(x+1,y), (x-1,y), (x,y+1), (x,y-1)]
           edges = filter (\coord -> let (N _ i' _ _) = (mapped ! coord) in i' == i + 1 || i' == i || i'<i) adj
-    -- where option = [(x',y') | x' <- [(x-1)..(x+1)], y<-[(y-1)..(y+1)], guards! ] not diag!
 
 getCoord :: Node -> Coordinate
 getCoord (N c i t ns) = c
@@ -74,7 +67,7 @@ dfsish' start = dfsish [start] [start] 0
 
 dfsish :: [Coordinate] -> [Coordinate] -> Int -> Map Coordinate Node -> Coordinate ->  Int
 dfsish visited lastRound roundNo mapped end | end `elem` visited = roundNo
-                                            | null lastRound     = error ("Can't continue " ++ (show roundNo)) -- ++ " (44,28): " ++ show ((44,28) `elem` visited) ++ " " ++ (sort $ map (getHeight . (mapped !)) visited)) -- ++ show visited)
+                                            | null lastRound     = maxBound
                                             | otherwise          = dfsish (visited++connected) connected  (roundNo+1) mapped end
                                     where connected = dedup $ filter (`notElem` visited) $ concatMap (getConnected . (mapped !) ) lastRound
 
